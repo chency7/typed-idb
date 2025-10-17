ae246c0067be81f065a68b35540890f27149c143
chency <1611804875@qq.com>
2025-10-17 16:59:10 +0800

    🔨 chore: 修复装饰器类型问题

--- src/decorators/transaction.ts
+++ src/decorators/transaction.ts
@@ -16,53 +16,45 @@
  */
 
 interface HasTransactionProvider {
-    dbManager?: { getTransaction: (storeNames: string | string[], mode: IDBTransactionMode) => IDBTransaction };
-    db?: { getTransaction: (storeNames: string | string[], mode: IDBTransactionMode) => IDBTransaction };
+    dbManager?: TransactionProvider;
+    db?: TransactionProvider;
 }
 
-export function transaction(storeNames: string | string[], mode: IDBTransactionMode = 'readwrite') {
-    return function (target: object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<any>>) {
+export function transaction<TArgs extends unknown[], TResult = unknown>(storeNames: string | string[], mode: IDBTransactionMode = 'readwrite') {
+    return function (target: object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<(...args: TArgs) => Promise<TResult> | TResult>) {
         const originalMethod = descriptor.value;
-        if (!originalMethod) return descriptor;
+        if (!originalMethod) return descriptor as TypedPropertyDescriptor<(...args: TArgs) => Promise<TResult>>;
 
-        descriptor.value = async function (this: HasTransactionProvider, ...args: any[]) {
-            const provider = this.dbManager ?? this.db;
+        descriptor.value = async function (this: HasTransactionProvider, ...args: TArgs): Promise<TResult> {
+            const provider: TransactionProvider | undefined = this.dbManager ?? this.db;
             if (!provider || typeof provider.getTransaction !== 'function') {
                 throw new Error('未找到 getTransaction 提供者，请在类中声明 db 或 dbManager 属性');
             }
 
             const tx = provider.getTransaction(storeNames, mode);
-            return new Promise<any>((resolve, reject) => {
+            return new Promise<TResult>((resolve, reject) => {
                 try {
                     // 设置活动事务，方法内部的 db.getObjectStore 将复用同一事务
-                    if (typeof (provider as any).setActiveTransaction === 'function') {
-                        (provider as any).setActiveTransaction(tx);
-                    }
+                    provider.setActiveTransaction?.(tx);
                     const maybePromise = originalMethod.apply(this, args);
                     Promise.resolve(maybePromise).then((val) => {
-                        if (typeof (provider as any).clearActiveTransaction === 'function') {
-                            (provider as any).clearActiveTransaction();
-                        }
-                        tx.oncomplete = () => resolve(val);
+                        provider.clearActiveTransaction?.();
+                        tx.oncomplete = () => resolve(val as TResult);
                         tx.onerror = () => reject(tx.error || new Error('事务执行失败'));
                     }).catch((err) => {
                         try { tx.abort(); } catch { /* ignore */ }
-                        if (typeof (provider as any).clearActiveTransaction === 'function') {
-                            (provider as any).clearActiveTransaction();
-                        }
+                        provider.clearActiveTransaction?.();
                         reject(err);
                     });
                 } catch (err) {
                     try { tx.abort(); } catch { /* ignore */ }
-                    if (typeof (provider as any).clearActiveTransaction === 'function') {
-                        (provider as any).clearActiveTransaction();
-                    }
+                    provider.clearActiveTransaction?.();
                     reject(err);
                 }
             });
         };
 
-        return descriptor;
+        return descriptor as TypedPropertyDescriptor<(...args: TArgs) => Promise<TResult>>;
     };
 }
 
@@ -71,3 +63,10 @@ export function transaction(storeNames: string | string[], mode: IDBTransactionM
 export function performTransaction(operation: () => void) {
     operation();
 }
+
+// 更精准的事务提供者类型
+type TransactionProvider = {
+  getTransaction: (storeNames: string | string[], mode: IDBTransactionMode) => IDBTransaction;
+  setActiveTransaction?: (tx: IDBTransaction) => void;
+  clearActiveTransaction?: () => void;
+};
